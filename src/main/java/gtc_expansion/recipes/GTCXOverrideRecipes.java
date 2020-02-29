51ae553c6cb3b4374f6254efb4e66f3f645bc443
CrossVas <cross.wanted.1.4@gmail.com>
2020-02-29 16:39:17 +0200

    Formatting

--- src/main/java/gtc_expansion/recipes/GTCXOverrideRecipes.java
+++ src/main/java/gtc_expansion/recipes/GTCXOverrideRecipes.java
@@ -28,8 +28,8 @@ public class GTCXOverrideRecipes {
 	static GTRecipeCraftingHandler recipe;
 	
 	public static void initGTCXOverride() {
-		if (Loader.isModLoaded(GTValues.MOD_ID_IC2_EXTRAS) && GTConfig.modcompat.compatIc2Extras) {
-			GTCExpansion.logger.info("Messing with IC2_Extras!");
+	if (Loader.isModLoaded(GTValues.MOD_ID_IC2_EXTRAS) && GTConfig.modcompat.compatIc2Extras) {
+	GTCExpansion.logger.info("Messing with IC2_Extras!");
             GTRecipeCraftingHandler.removeRecipe(GTValues.MOD_ID_IC2_EXTRAS, "shapeless_item.itemdustbronze_-1753288283");
             ClassicRecipes.macerator.removeRecipe(input("oreBauxite", 1));
             ClassicRecipes.macerator.removeRecipe(input("oreIridium", 1));
@@ -42,7 +42,7 @@ public class GTCXOverrideRecipes {
             GTRecipeCraftingHandler.overrideGTRecipe(GTValues.MOD_ID_IC2_EXTRAS, "shaped_tile.extruder_704871140", GTMaterialGen.getModItem(GTValues.MOD_ID_IC2_EXTRAS, "extruder"), "SCS", "cMc", "SCS", 'C', circuit, 'S', casing, 'M', machineBlock, 'c', GTMaterialGen.getModItem(GTValues.MOD_ID_IC2_EXTRAS, "coil"));
 		}
 		if (Loader.isModLoaded(GTValues.MOD_ID_FORESTRY)) {
-			GTCExpansion.logger.info("Messing with Forestry!");
+		GTCExpansion.logger.info("Messing with Forestry!");
 			String[] forestryPlanksType = { "_larch", "_teak", "_acacia", "_lime", "_chestnut", "_wenge", "_baobab", "_sequoia", "_kapok", "_ebony", "_mahogany", "_balsa", "_willow", "_walnut", "_greenheart", "_cherry", "_mahoe", "_poplar", "_palm", "_papaya", "_pine", "_plum", "_maple", "_citrus", "_giganteum", "_ipe", "_padauk", "_cocobolo", "_zebrawood", "_oak", "_spruce", "_birch", "_jungle", "_acacia", "_dark_oak" };
     		for (int i = 0; i < forestryPlanksType.length; i++) {
     			((ForgeRegistry)ForgeRegistries.RECIPES).remove(new ResourceLocation("forestry", "planks" + forestryPlanksType[i]));
@@ -53,14 +53,14 @@ public class GTCXOverrideRecipes {
     		((ForgeRegistry)ForgeRegistries.RECIPES).remove(new ResourceLocation("forestry", "gear_tin"));
 		}
 		if (Loader.isModLoaded(GTValues.MOD_ID_TFOREST) && GTConfig.modcompat.compatTwilightForest) {
-			GTCExpansion.logger.info("Messing with The Twilight Forest!");
+		GTCExpansion.logger.info("Messing with The Twilight Forest!");
 			String[] twilightPlanksType = { "wood/twilight_oak_", "wood/canopy_", "wood/mangrove_", "wood/darkwood_", "wood/time_", "wood/trans_", "wood/mine_", "wood/sort_" }; 
     		for (int i = 0; i < twilightPlanksType.length; i++) {
     			((ForgeRegistry)ForgeRegistries.RECIPES).remove(new ResourceLocation("twilightforest", twilightPlanksType[i] + "planks"));
     		}
 		}
 		if (Loader.isModLoaded(GTValues.MOD_ID_THERMAL) && GTConfig.modcompat.compatThermal) {
-			GTCExpansion.logger.info("Messing with Thermal Mods!");
+		GTCExpansion.logger.info("Messing with Thermal Mods!");
             recipe.overrideGTRecipe("thermalexpansion", "frame", GTMaterialGen.getModItem("thermalexpansion", "frame"), "SGS", "GTG", "SGS", 'S', "ingotSteel", 'G', "blockGlass", 'T', "gearTin");
             for (GTMaterial mat : GTMaterial.values()) {
                 ThermalExpansionHelper.removeCompactorPlateRecipe(GTMaterialGen.getIngot(mat, 1));	
@@ -68,12 +68,12 @@ public class GTCXOverrideRecipes {
             }
 		}
 		if (Loader.isModLoaded("gravisuit")) {
-			GTCExpansion.logger.info("Tweaking GraviSuit");
+		GTCExpansion.logger.info("Tweaking GraviSuit");
             GTRecipeCraftingHandler.overrideGTRecipe("gravisuit", "shaped_item.advanceddiamondchainsaw_-416372460", GTMaterialGen.getModItem("gravisuit", "advancedchainsaw"), " SS", "SCS", "BS ", 'S', RecipeHelper.tungstenSteel, 'C', GTCXItems.diamondChainsaw, 'B', GTItems.lithiumBattery);
             GTRecipeCraftingHandler.overrideGTRecipe("gravisuit", "shaped_item.advanceddrill_1408250051", GTMaterialGen.getModItem("gravisuit", "advanceddrill"), " S ", "SDS", "SBS", 'S', RecipeHelper.tungstenSteel, 'D', Ic2Items.diamondDrill, 'B', GTItems.lithiumBattery);
 		}
 		if (Loader.isModLoaded("railcraft")) {
-			GTCExpansion.logger.info("Wait a minute... RailCraft?! No way!!");
+		GTCExpansion.logger.info("Wait a minute... RailCraft?! No way!!");
 		}
 		GTCExpansion.logger.info("Vanilla... Bruh...");
     		String[] vanillaPlanksType = { "oak_", "spruce_", "birch_", "jungle_", "acacia_", "dark_oak_" };
